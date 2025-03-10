eea127e1994d6bedc3b5072de0addf9eaa6a5bed
CodingF0X <roody9a@gmail.com>
2025-03-10 13:49:20 +0100

    ..

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
