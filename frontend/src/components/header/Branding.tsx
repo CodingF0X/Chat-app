0e1bd118bfcecd99cdff2e692901ce04012a2416
CodingF0X <roody9a@gmail.com>
2025-03-26 19:36:10 +0100

    --feat_frontend- added Navbar

---
+++ frontend/src/components/header/Branding.tsx
@@ -0,0 +1,29 @@
+import DraftsIcon from '@mui/icons-material/Drafts';
+import { Typography } from '@mui/material';
+import Routes from '../Routes';
+const Branding = () => {
+  return (
+    <>
+      <DraftsIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+      <Typography
+        variant="h6"
+        noWrap
+        component="a"
+        onClick={()=> Routes.navigate('/')}
+        sx={{
+          mr: 2,
+          display: { xs: "none", md: "flex" },
+          fontFamily: "monospace",
+          fontWeight: 700,
+          letterSpacing: ".3rem",
+          color: "inherit",
+          textDecoration: "none",
+        }}
+      >
+        Foxie
+      </Typography>
+    </>
+  );
+};
+
+export default Branding;
