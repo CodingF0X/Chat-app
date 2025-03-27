b81b7646d1f1e6016d7c263ef0a648807356a1db
CodingF0X <roody9a@gmail.com>
2025-03-27 10:46:01 +0100

    --feat_frontend- conditionally render user settings

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,24 +1,33 @@
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
-import { Toolbar } from "@mui/material";
+import { Toolbar, Typography } from "@mui/material";
 import Branding from "./Branding";
 import Mobile_Nav from "./mobile/Mobile_Nav";
 import Mobile_Branding from "./mobile/Mobile_Branding";
 import Desktop_Nav from "./Desktop_Nav";
 import Settings from "./Settings";
+import { useGetMe } from "../../hooks/useGetMe";
+import Routes from "../Routes";
 
-const pages = ["Products", "Pricing", "Blog"];
+const pages = [""];
 
 const Header = () => {
+  const { data:user } = useGetMe();
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{color:'white', background:'orange'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Branding />
           <Mobile_Nav pages={pages} />
           <Mobile_Branding />
           <Desktop_Nav pages={pages} />
-          <Settings />
+          {user ? <Settings /> : (
+            <Typography component='a' onClick={()=> Routes.navigate('/login')}>
+              Login
+
+            </Typography>
+          )}
+          
         </Toolbar>
       </Container>
     </AppBar>
