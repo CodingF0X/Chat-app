435afd4d8286f7f3fbde3579081554f7fc7a3a5c
CodingF0X <roody9a@gmail.com>
2025-04-08 12:36:10 +0200

    --feat_api: fixed circular dependency between Chat and Message modules

--- backend/src/chats/chats.module.ts
+++ backend/src/chats/chats.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsResolver } from './chats.resolver';
 import { ChatRepository } from './chat.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Chat, ChatSchema } from './entities/chat.entity';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
@@ -13,7 +14,9 @@ import { Chat, ChatSchema } from './entities/chat.entity';
         schema: ChatSchema,
       },
     ]),
+    forwardRef(() => MessagesModule),
   ],
   providers: [ChatsResolver, ChatsService, ChatRepository],
+  exports: [ChatRepository],
 })
 export class ChatsModule {}
