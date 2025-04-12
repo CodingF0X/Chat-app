2365ddc05d989b4fda4e200ce59ce1807b88140a
CodingF0X <roody9a@gmail.com>
2025-04-12 13:06:02 +0200

    --feat_frontend: added the websocket url

--- frontend/src/constants/apollo-client.ts
+++ frontend/src/constants/apollo-client.ts
@@ -1,8 +1,11 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import { API_URL } from "./urls";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { API_URL, API_WS_URL } from "./urls";
 import { onError } from "@apollo/client/link/error";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { exludedRoutes } from "./excluded-routes";
 import onLogout from "../utils/onLogout";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const logoutLink = onError((err) => {
   if (
@@ -21,9 +24,29 @@ it points to the GraphQL endpoint http://localhost:3000/graphql
 */
 const httpLink = new HttpLink({ uri: `${API_URL}/graphql` });
 
+const wsLink = new GraphQLWsLink(
+  createClient({ url: `ws://${API_WS_URL}/graphql` })
+);
+
+
+// now to determine whether the operation is GQL query or subscription,
+// we get the operation definition, which returns a boolean value,
+// if the left operand is true, then it's a subscription, otherwise it's a query.
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return ( //boolean
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,// Left operand: if the return was true then we consume this link
+  httpLink // Right operand: else we consume the normal httpLink
+);
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: logoutLink.concat(httpLink),
+  link: logoutLink.concat(link),
 });
 
 /* 
