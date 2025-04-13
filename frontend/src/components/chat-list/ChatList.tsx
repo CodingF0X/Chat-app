90ee63a575d54d837d8b2fb0787d2534c9da2419
CodingF0X <roody9a@gmail.com>
2025-04-13 20:28:07 +0200

    --feat_frontend- refactor: get chats list right from the cache and sort them by the latest message

--- frontend/src/components/chat-list/ChatList.tsx
+++ frontend/src/components/chat-list/ChatList.tsx
@@ -7,12 +7,15 @@ import { useEffect, useState } from "react";
 import ChatListAdd from "./chat-list-add/ChatListAdd";
 import useGetChats from "../../hooks/useGetChats";
 import usePath from "../../hooks/usePaths";
+import useMessageCreated from "../../hooks/useMessageCreated";
 
 const ChatList = () => {
   const [openAddChatModal, setOpenAddChatModal] = useState(false);
   const [selected, setSelected] = useState("");
   const { data } = useGetChats();
   const { path } = usePath();
+  const chatsIds = data?.Find_Chats.map((chat) => chat._id) || [];
+  useMessageCreated({ chatIds: chatsIds });
 
   useEffect(() => {
     const selectedPath = path.split("chats/");
@@ -38,9 +41,24 @@ const ChatList = () => {
             overflow: "auto",
           }}
         >
-          {data?.Find_Chats.map((chat) => (
-            <ChatListItem key={chat._id} chat={chat} selected = {selected === chat._id}/>
-          )).reverse()}
+          {data?.Find_Chats &&
+            [...data.Find_Chats]
+              .sort((chatA, chatB) => {
+                if (!chatA.latestMessage || !chatB.latestMessage) {
+                  return -1; // Handle cases where latestMessage is undefined
+                }
+                return (
+                  new Date(parseInt(chatA.latestMessage?.createdAt)).getTime() -
+                  new Date(parseInt(chatB.latestMessage?.createdAt)).getTime()
+                );
+              })
+              .map((chat) => (
+                <ChatListItem
+                  key={chat._id}
+                  chat={chat}
+                  selected={selected === chat._id}
+                />
+              )).reverse()}
           <Divider variant="inset" component="li" />
         </List>
       </Stack>
