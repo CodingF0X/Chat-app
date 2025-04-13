9c06d0f1b3b9eafebec81c1d14e2a64afd77afc0
CodingF0X <roody9a@gmail.com>
2025-04-13 20:11:33 +0200

    --feat_frontend- refactor: consumed the messages directly from Apollo cache

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -14,18 +14,16 @@ import useSendMessage from "../../hooks/useSendMessage";
 import { useEffect, useRef, useState } from "react";
 import Message from "./message/Message";
 import useGetMessages from "../../hooks/useGetMessages";
-import useMessageCreated from "../../hooks/useMessageCreated";
-import type { Message as msgGQL } from "../../gql/graphql";
 
 const Chat = () => {
   const params = useParams<{ _id: string }>();
   const chatId = params._id!;
   const { data } = useGetChat({ id: chatId }); // ! to asserts non-null
+
   const [message, setMessage] = useState<string>("");
   const [createMessage] = useSendMessage(chatId);
-  const { data: existingMessages } = useGetMessages({ chatId });
-  const { data: latestMessage } = useMessageCreated({ chatId });
-  const [messages, setMessages] = useState<msgGQL[]>([]);
+  const { data: messages } = useGetMessages({ chatId });
+
   const divRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
 
@@ -36,25 +34,7 @@ const Chat = () => {
 
   useEffect(() => {
     scrollToBottom();
-  }, [location, existingMessages, messages]);
-
-  useEffect(() => {
-    if (existingMessages) {
-      setMessages(existingMessages.Get_All_Messages);
-    }
-  }, [existingMessages]);
-
-  useEffect(() => {
-    //to check if the already existing messages have the _id of the newly created one.
-    // if no, then we add the newly creted one to the array of messages.
-    const existingLatestMessages = messages[messages.length - 1]?._id;
-    if (
-      latestMessage?.Message_Created &&
-      latestMessage?.Message_Created._id !== existingLatestMessages
-    ) {
-      setMessages([...messages, latestMessage?.Message_Created]);
-    }
-  }, [latestMessage, messages]);
+  }, [location.pathname, messages]);
 
   const handleSendMessage = async () => {
     await createMessage({
@@ -72,7 +52,7 @@ const Chat = () => {
         {data?.Find_Single_Chat.name}
       </Typography>
       <Box overflow={"auto"} sx={{ maxHeight: "80vh" }}>
-        {messages
+        {messages?.Get_All_Messages
           ?.slice() // Create a copy to avoid mutating the original array
           .sort((messageA, messageB) => {
             // Convert createdAt strings to Date objects and compare
