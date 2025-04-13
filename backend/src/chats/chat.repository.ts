cfb4af116cf9420d7757056d68741f816ebc4b9f
CodingF0X <roody9a@gmail.com>
2025-04-13 10:30:18 +0200

    --feat_api- pass ChatDocument as Type

--- backend/src/chats/chat.repository.ts
+++ backend/src/chats/chat.repository.ts
@@ -1,14 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { AbstractRepository } from 'src/common/database/abstract.repository';
-import { Chat } from './entities/chat.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ChatDocument } from './entities/chat.document';
 
 @Injectable()
-export class ChatRepository extends AbstractRepository<Chat> {
+export class ChatRepository extends AbstractRepository<ChatDocument> {
   protected readonly logger = new Logger(ChatRepository.name);
 
-  constructor(@InjectModel(Chat.name) chatModel: Model<Chat>) {
+  constructor(@InjectModel(ChatDocument.name) chatModel: Model<ChatDocument>) {
     super(chatModel);
   }
 }
