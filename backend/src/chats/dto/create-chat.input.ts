028fcf463fb1a9224b00df53c5e9f1be21006706
CodingF0X <roody9a@gmail.com>
2025-04-13 10:32:02 +0200

    --feat_api-  refactor temorarily remove the fields

--- backend/src/chats/dto/create-chat.input.ts
+++ backend/src/chats/dto/create-chat.input.ts
@@ -1,33 +1,13 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
-import { Transform } from 'class-transformer';
 import {
-  IsArray,
-  IsBoolean,
   IsNotEmpty,
-  IsOptional,
   IsString,
 } from 'class-validator';
 
 @InputType()
 export class CreateChatInput {
-  @Field(() => Boolean, { description: 'Example field (placeholder)' })
-  @Transform(({ value }) => value === 'true')
-  @IsBoolean()
-  isPrivate: boolean;
-
-  @Field(() => [String], {
-    nullable: true,
-    description: 'Array of chat participants',
-  })
-  @IsArray()
-  @IsString({ each: true })
-  @IsNotEmpty({ each: true })
-  @IsOptional()
-  participants: string[];
-
-  @Field(() => String, { nullable: true, description: 'Chat name' })
+  @Field(() => String, { description: 'Chat name' })
   @IsString()
-  @IsOptional()
   @IsNotEmpty()
   name: string;
 }
