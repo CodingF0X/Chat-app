71c04e8dd1e249c77d0a0ed9b48b23199fa3ec63
CodingF0X <roody9a@gmail.com>
2025-04-15 12:48:45 +0200

    --feat_api- refactor: protect the route with JWT

--- backend/src/chats/messages/messages.controller.ts
+++ backend/src/chats/messages/messages.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
   @Get('count')
+  @UseGuards(JwtAuthGuard)
   async messagesCount(@Query('chatId') chatId: string) {
     return await this.messagesService.countDocs(chatId);
   }
