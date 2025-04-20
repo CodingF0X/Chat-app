805d0591a2b9ff1eef89177ffd9503d5dfd5db48
CodingF0X <roody9a@gmail.com>
2025-04-20 09:18:15 +0200

    --feat_api- seperated User entity from GQL schema type

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,15 +3,15 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { UserRepository } from './users.repository';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModel, User } from './entities/user.schema';
 import { UsersController } from './users.controller';
 import { S3Module } from 'src/common/s3/s3.module';
+import { UserDocument, UserModel } from './entities/user.document';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {
-        name: User.name,
+        name: UserDocument.name,
         schema: UserModel,
       },
     ]),
